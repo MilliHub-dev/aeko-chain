23698c20c20bc05c68ccb8fd47e38e46a7787ffe
Surdma <devsurdma@gmail.com>
2026-09-24 18:13:52 +0100

    refactor(explorer): simplify testnet console wrapper

--- apps/explorer/web/src/components/NetworkConsoleModal.jsx
+++ apps/explorer/web/src/components/NetworkConsoleModal.jsx
@@ -2,10 +2,7 @@ import NetworkConsoleModalV2 from './NetworkConsoleModalV2';
 import { getNetworkConfig } from '../utils/networkConfig';
 
 export default function NetworkConsoleModal(props) {
-  const networkKey = String(props.network || '').toLowerCase().startsWith('mainnet')
-    ? 'mainnet'
-    : 'testnet';
-  const config = getNetworkConfig(networkKey);
+  const config = getNetworkConfig();
 
   return (
     <NetworkConsoleModalV2
